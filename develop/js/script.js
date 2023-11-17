64ebef190738744e683033d4bb25741b5788bfdd
Shibiiii <purplejohn13@gmail.com>
2023-11-16 19:53:54 -0800

    Test fetch requests written

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,21 +1,55 @@
-// For Spotify
-// client id = ebd0b6e8892a413c9969598cb0b48b4d
-// client secret = 6f53037bb38e4c949cb39d4ae391a788
-// redirect URI = http://localhost/
 
-// For Youtube
-// Api key = AIzaSyAMIfQahCTVQNvVZvcFzUEI9E_Atky5qyI
 
-function getApi() {
-  var requestUrl = 'https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?part=snippet&order=viewCount&q=skateboarding+dog&type=video&videoDefinition=high';
-  fetch(requestUrl, {
-    mode: 'no-cors'
-  })
-    .then(function (response) {
-      return response.json();
-})
-};
-console.log(getApi());
+async function getApiTranslate() {
+
+    const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/gzip',
+        'X-RapidAPI-Key': '82efe9f845msh0cad063e477d0d4p1761a8jsn1d472168cc42',
+        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+      },
+      body: new URLSearchParams({
+        q: 'Hello, world!',
+        format: 'html',
+        target: 'es',
+        source: 'en'
+      })
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(getApiTranslate());
+
+  async function getApiLyrics() {
+    const url = 'https://genius-song-lyrics1.p.rapidapi.com/search/?q=from%20the%20start&per_page=1&page=1&text_format=html';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '82efe9f845msh0cad063e477d0d4p1761a8jsn1d472168cc42',
+        'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(getApiLyrics());
+
+
 
 
 
