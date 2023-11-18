36cde026f5eddc5151188f4f9d38d201277515eb
John Lopez <purplejohn13@gmail.com>
2023-11-18 13:24:05 -0800

    removed unnecessary text in html

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,33 +1,33 @@
 
 
-async function getApiTranslate() {
+// async function getApiTranslate() {
 
-    const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
-    const options = {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Accept-Encoding': 'application/gzip',
-        'X-RapidAPI-Key': '82efe9f845msh0cad063e477d0d4p1761a8jsn1d472168cc42',
-        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-      },
-      body: new URLSearchParams({
-        q: 'Hello, world!',
-        format: 'html',
-        target: 'es',
-        source: 'en'
-      })
-    };
+//     const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+//     const options = {
+//       method: 'POST',
+//       headers: {
+//         'content-type': 'application/x-www-form-urlencoded',
+//         'Accept-Encoding': 'application/gzip',
+//         'X-RapidAPI-Key': '82efe9f845msh0cad063e477d0d4p1761a8jsn1d472168cc42',
+//         'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+//       },
+//       body: new URLSearchParams({
+//         q: 'Hello, world!',
+//         format: 'html',
+//         target: 'es',
+//         source: 'en'
+//       })
+//     };
     
-    try {
-      const response = await fetch(url, options);
-      const result = await response.text();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  console.log(getApiTranslate());
+//     try {
+//       const response = await fetch(url, options);
+//       const result = await response.text();
+//       console.log(result);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+//   console.log(getApiTranslate());
 
   async function getApiLyrics() {
     const url = 'https://genius-song-lyrics1.p.rapidapi.com/search/?q=from%20the%20start&per_page=1&page=1&text_format=html';
