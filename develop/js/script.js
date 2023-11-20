593e9cbdb31d2fd3b37c48454c3b7a293ed350cc
John Lopez <purplejohn13@gmail.com>
2023-11-19 16:33:56 -0800

    basic java for weather search written

--- develop/js/script.js
+++ develop/js/script.js
@@ -26,12 +26,39 @@ searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
 var cityName = document.querySelector('#cityname');
 var countryName = document.querySelector('#countryname');
+var index = 1;
 
 btn.addEventListener('click', function(event) {
     event.preventDefault();
-    
+    startNewSearch();
+    var city = cityName.value;
+    saveCityName(city);
+    getApiData(city);
 }
-)
+);
+
+function getApiData(city) {
+    var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city +'&appid=a9f48eaca2ef1bc28989582adf1daa56';
+
+    fetch(requestUrl).then(function(response) {
+        return response.json(); 
+    }).then(function(data) {
+        var secondUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat +'&lon=' + data[0].lon + '&appid=a9f48eaca2ef1bc28989582adf1daa56&units=imperial';
+
+        fetch(secondUrl).then(function (response){
+            return response.json();
+        }).then(function(data) {
+            for(var i=0; i<=1; i++) {
+            var forecast = document.querySelector('#info2' + (i+2));
+            var createText = document.createElement('p');
+            var tempData = data.list[i*8].main.temp;
+
+            createText.innerHTML = tempData + forecast;
+            }
+        })
+
+    })
+}
 
 
 
